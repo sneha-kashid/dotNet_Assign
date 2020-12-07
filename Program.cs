09b0889a9b9de71e7cae31d7e636dc83d2a4655b
sneha-kashid <65266245+sneha-kashid@users.noreply.github.com>
2020-12-07 20:23:51 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,147 +4,107 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment1
+namespace TwoDArray
 {
     class Program
     {
+        static void Main1()
+        {
+            int[,] arr = new int[5, 3];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("Enter marks for student number {0} and subject {1} : " , i, j);
+                  
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+
+                }
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("Marks for student number {0} and subject {1} : {2}", i, j,arr[i,j]);
+                   
+
+                }
+            }
+        }
+
+        static void Main2()
+        {
+            Console.WriteLine("Enter Number of student");
+            int Stud = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the marks");
+            int Marks = Convert.ToInt32(Console.ReadLine());
+            int[,] arrS = new int[Stud, Marks];
+
+            for (int i = 0; i < Stud; i++)
+            {
+                for (int j = 0; j < Marks; j++)
+                {
+                    Console.Write("Enter marks for student number {0} and subject {1} : ", i, j);
+
+                    arrS[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < Stud; i++)
+            {
+                for (int j = 0; j < Marks; j++)
+                {
+                    Console.WriteLine("Marks for student number {0} and subject {1} : {2}", i, j, arrS[i, j]);
+
+
+                }
+            }
+        }
+
         static void Main()
         {
-            //Employee e = new Employee();
-            //e.Basic=30;
-            //Console.WriteLine(e.Basic);
-            //e.Name="neha";
-            //Console.WriteLine(e.Name);
-            //e.DeptNo = 1;
-            //Console.WriteLine(e.DeptNo);
-            //Employee.EmpNo=0;
-            //Console.WriteLine(Employee.EmpNo);
-            //Console.WriteLine(Employee.EmpNo);
+            //Console.WriteLine("Enter Number of student");
+            //int Stud = Convert.ToInt32(Console.ReadLine());
 
-           // Console.WriteLine(e.GetNetSalary());
+            //Console.WriteLine("Enter the marks");
+            //int Marks = Convert.ToInt32(Console.ReadLine());
+            int[,] arrS = new int[5, 2];
+
+            Console.WriteLine(arrS.Rank);
+            Console.WriteLine(arrS.Length);
+            Console.WriteLine(arrS.GetLowerBound(0));
+            Console.WriteLine(arrS.GetLowerBound(1));
+            Console.WriteLine(arrS.GetUpperBound(0));
+            Console.WriteLine(arrS.GetUpperBound(1));
 
 
-
-            Employee o1 = new Employee("Amol", 34, 10);
-
-            // Console.WriteLine(o1.EmpNo+" " +o1.DeptNo+" "+o1.Basic );
-            //Console.WriteLine(o1.GetNetSalary());
-            Console.WriteLine(o1.EmpNo);
-            Employee o2 = new Employee("Amol", 46);
-            Console.WriteLine(o2.EmpNo);
-            Employee o3 = new Employee("Amol");
-            Console.WriteLine(o3.EmpNo);
-            Employee o4 = new Employee();
-            Console.WriteLine(o4.EmpNo);
-            // o3.EmpNo--;
-
-            // Employee o3 = new Employee("Amol");
-            Console.WriteLine(o4.EmpNo);
-            Console.WriteLine(o3.EmpNo);
-            Console.WriteLine(o2.EmpNo);
-            Console.WriteLine(o1.EmpNo);
-
-        }
-    }
-
-    class Employee
-    {
-
-        public Employee()
-        {
-            emp_no=++var;
-        }
-
-        public Employee(string Name)
-        {
-            this.Name = Name;
-            emp_no=++var;
-        }
-
-        public Employee(string Name,int Basic)
-        {
-            emp_no=++var;
-            this.Name = Name;
-            this.Basic = Basic;
-        }
-        public Employee(string Name, int Basic, short DeptNo)
-        {
-            emp_no=++var;
-            this.Name = Name;
-            this.Basic = Basic;
-            this.DeptNo = DeptNo;
-            
-        }
-        private string name;
-        public string Name
-        {
-            set
+            for (int i = 0; i < arrS.GetLength(0) ; i++)
             {
-                if(value != null)
-                    name = value;
-                else
-                    Console.WriteLine("name can't be null");
+                for (int j = 0; j <arrS.GetLength(1); j++)
+                {
+                    Console.Write("Enter marks for student number {0} and subject {1} : ", i, j);
+
+                    arrS[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
             }
-            get
+
+            Console.WriteLine();
+
+            for (int i = 0; i < arrS.GetLength(0); i++)
             {
-                return name;
+                for (int j = 0; j < arrS.GetLength(1); j++)
+                {
+                    Console.WriteLine("Marks for student number {0} and subject {1} : {2}", i, j, arrS[i, j]);
+
+
+                }
             }
         }
-
-        private  int emp_no;
-        private static int var;
-        public  int EmpNo
-        {
-           
-            get
-            {
-             
-                return emp_no;
-            }
-            
-
-        }
-
-        private int basic;
-        public int Basic
-        {
-            set
-            {
-                if (value > 20 && value < 50)
-                    basic = value;
-                else
-                    Console.WriteLine("invalid");
-            }
-            get
-            {
-                return basic;
-            }
-        }
-
-        private short dept_no;
-        public short DeptNo
-        {
-            set
-            {
-                if (value > 0)
-                    dept_no = value;
-                else
-                    Console.WriteLine("invalid");
-            }
-            get
-            {
-                return dept_no;
-            }
-        }
-
-
-        public float GetNetSalary()
-        {
-            float sal = Basic + 4000 + 1000;
-            return sal;
-        }
-
-
 
     }
 }
